602e73c634ecdf05956ea83f2c297cd5a22a4e57
beom101 <cksrn7188@gmail.com>
2026-06-01 17:37:49 +0900

    260601_sp_day5_BoardMovieMember

--- 260528_Sp_D1/todo_table_create.sql
+++ 260528_Sp_D1/todo_table_create.sql
@@ -72,9 +72,57 @@ INSERT INTO
 	board(no,title,content,nickname,regdate,hit) 
 	values(board_seq.nextval,'제목','내용','홍길동',sysdate,0);
 
+
+UPDATE board SET hit = hit+1 WHERE NO=3;
+
 SELECT * FROM board;
 ROLLBACK;
 
 COMMIT;
 
+SELECT * FROM MEMBER;
+CREATE TABLE member (
+    no NUMBER CONSTRAINT member_no_pk PRIMARY KEY,
 
+    user_id VARCHAR2(50)
+        CONSTRAINT member_user_id_nn NOT NULL,
+
+    user_name VARCHAR2(50)
+        CONSTRAINT member_user_name_nn NOT NULL,
+
+    user_pw VARCHAR2(100)
+        CONSTRAINT member_user_pw_nn NOT NULL,
+
+    email VARCHAR2(100)
+        CONSTRAINT member_email_nn NOT NULL,
+
+    phone VARCHAR2(20)
+        CONSTRAINT member_phone_nn NOT NULL,
+
+    address VARCHAR2(300)
+        CONSTRAINT member_address_nn NOT NULL,
+
+    reg_date TIMESTAMP DEFAULT SYSTIMESTAMP,
+
+    CONSTRAINT member_user_id_uk UNIQUE (user_id),
+
+    CONSTRAINT member_email_uk UNIQUE (email)
+);
+
+
+CREATE SEQUENCE member_seq
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
+
+INSERT INTO MEMBER VALUES
+(member_seq.nextval,'jjang051','장성호','1234','jjang051','010-1111-1111','고양시 장항동 11-11',sysdate);
+
+SELECT * FROM MEMBER;
+SELECT count(*) FROM MEMBER WHERE USER_ID='aaa'; -- DB에서 Error 터지면 검증하는 방법?
+SELECT count(*) FROM MEMBER WHERE email='gilgildong@naver.com'; -- DB에서 Error 터지면 확인하는 방법?
+ROLLBACK;
+COMMIT;
+
+SELECT count(*) FROM MEMBER WHERE user_id='aaa' AND user_pw='1234';
