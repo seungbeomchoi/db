ae235c8489f54fa0d20704b2de4c6c80c50a2142
beom101 <cksrn7188@gmail.com>
2026-06-10 17:40:40 +0900

    260610_mini_project_start

---
+++ 260610_minipro/minipro_create.sql
@@ -0,0 +1,14 @@
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE; --sys에서
+--사용자 생성 명령어
+CREATE USER minipro IDENTIFIED BY 1234; 
+--접속할 수 있는 권한 부여 명령어
+GRANT CREATE SESSION TO minipro; 
+--테이블 만들 수 있는 권한 부여 명령어
+GRANT CREATE TABLE TO minipro; 
+--시퀀스 만들 수 있는 권한 부여 명령어
+GRANT CREATE SEQUENCE TO minipro; 
+--view 만들 수 있는 권한 부여 명령어
+GRANT CREATE VIEW TO minipro; 
+-- users tablespace에 데이터를 저장할 권한 부여하는 명령어
+ALTER USER minipro quota unlimited ON users; 
+COMMIT;
